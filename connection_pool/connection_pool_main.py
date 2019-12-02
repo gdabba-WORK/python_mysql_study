5b200992719a278e4b984465a6e6028cca2409f6
Builder <kgdfather@gmail.com>
2019-12-02 12:26:26 +0900

    --connection_pool package-- connection_pool_main.py 변 connection_pool_study02.py 구현경

--- connection_pool/connection_pool_main.py
+++ connection_pool/connection_pool_main.py
@@ -1,5 +1,7 @@
 import inspect
 from connection_pool.connection_pool_study01 import DatabaseConnectionPool
+from connection_pool.connection_pool_study02 import ExplicitlyConnectionPool, get_implicitly_connection
+
 
 def connection_pool01():
     print("=={}()==".format(inspect.stack()[0][3]))
@@ -8,6 +10,27 @@ def connection_pool01():
     connection.close()
 
 
+def explicitly_connection_pool():
+    print("=={}()==".format(inspect.stack()[0][3]))
+    connectionPool = ExplicitlyConnectionPool.get_instance()
+    connection = connection_pool01().get_connection()
+    print(type(connection), connection)
+    connection.close()
+
+
+def implicitly_connection_pool():
+    print("=={}()==".format(inspect.stack()[0][3]))
+    connection = get_implicitly_connection()
+    print(type(connection), connection)
+    connection.close()
+
+
 if __name__ == "__main__":
-    for i in range(10):
-        connection_pool01()
+    # for i in range(10):
+    #     connection_pool01()
+    explicitly_connection_pool()
+    implicitly_connection_pool()
+
+    explicitly_connection_pool()
+    implicitly_connection_pool()
+
